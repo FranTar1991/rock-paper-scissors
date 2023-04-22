0a816db7681ec0cfedaca2051f12a3c27b719494
Frank Tardencilla <franktardencilla@gmail.com>
2023-04-22 14:40:15 -0400

    Add the game function

--- script.js
+++ script.js
@@ -8,34 +8,77 @@ function getComputerChoice(){
 function playRound(playerSelection, computerSelection){
     let _playerSelection = playerSelection.toLowerCase()
     let _computerSelection = computerSelection.toLowerCase()
-    let result = null
+    let winner = null
 
 
     switch (true) {
-        //if it's a tie
         case _playerSelection == _computerSelection:
-            result = `It's a tie ${_computerSelection} and ${_playerSelection} are the same`
+            winner = 0
             break
-        
         case _playerSelection == gameOptions[0] && _computerSelection == gameOptions[1]:
         case _playerSelection == gameOptions[1] && _computerSelection == gameOptions[2]:
         case _playerSelection == gameOptions[2] && _computerSelection == gameOptions[0]:
-            result = `You lose, ${_computerSelection} beats ${_playerSelection}` 
+            winner = 1
             break;
         case _computerSelection == gameOptions[0] && _playerSelection == gameOptions[1]:
         case _computerSelection == gameOptions[1] && _playerSelection == gameOptions[2]:
         case _computerSelection == gameOptions[2] && _playerSelection == gameOptions[0]:
-            result = `You win, ${_playerSelection} beats ${_computerSelection}`
+            winner = 2
             break;
     }
 
-    return result ?? `No official winner p: ${_playerSelection} and c: ${_computerSelection}`
+    return winner
 
 }
 
 function game(){
-    let playerSelection = "Rock"
-    let computerSelection = getComputerChoice()
-    console.log(playRound(playerSelection, computerSelection))
+let playerWins = 0
+let computerWins = 0
+
+    for(let i = 1; i<=5; i++){
+        let playerSelection = prompt(`Enter your option: Rock, Paper or Scissors`)
+        let computerSelection = getComputerChoice()
+        let winner = playRound(playerSelection, computerSelection)
+        if(winner != 0){
+            winner == 1 ? computerWins++ : playerWins++
+        }
+        logIndividualResult(winner, computerSelection, playerSelection)
+    }
+
+announceUltimateWinner(playerWins, computerWins)
+    
 }
 
+function announceUltimateWinner(playerWins, computerWins){
+    let announcement
+
+    if(playerWins == computerWins){
+        announcement = `It's a tie, the user won ${playerWins} games and the computer ${computerWins}`
+    } else{
+        playerWins > computerWins
+        ? announcement =`You won the game with ${playerWins} games and the computer ${computerWins}` 
+        : announcement = `The computer won the game with ${computerWins} games and you lost with ${playerWins}`
+    }
+
+    console.log(announcement)
+}
+
+function logIndividualResult(winner, computerSelection, playerSelection){
+    let results
+    switch(winner){
+     case 0:  
+        results = `It's a tie both selected: ${computerSelection}`
+        break
+     case 1: 
+        results =`You lose, ${computerSelection} beats ${playerSelection}` 
+        break
+     case 2: 
+        results =`You win, ${playerSelection} beats ${computerSelection}`
+        break
+     default:
+        results = `No official winner Your selection was: ${playerSelection} and the computer's selection was: ${computerSelection}`
+        break
+    }
+    
+    console.log(results)
+}
